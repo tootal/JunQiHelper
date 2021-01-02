fa2799ebd6d09677200f4d79fa5556f2ab009f18
tootal <tootal@yeah.net>
2021-01-03 00:29:07 +0800

    add mouseclickdialog layout

--- JunQiHelper_zh_CN.ts
+++ JunQiHelper_zh_CN.ts
@@ -22,8 +22,153 @@
 <context>
     <name>MouseClickDialog</name>
     <message>
-        <location filename="mouseclickdialog.ui" line="16"/>
+        <location filename="mouseclickdialog.ui" line="14"/>
         <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="22"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="31"/>
+        <source>Left Button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="41"/>
+        <source>Right Button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="50"/>
+        <source>Intervals</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="63"/>
+        <source>0.01</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="68"/>
+        <source>0.02</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="73"/>
+        <source>0.05</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="78"/>
+        <source>0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="83"/>
+        <source>0.2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="88"/>
+        <source>0.5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="93"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="98"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="103"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="108"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="113"/>
+        <source>10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="121"/>
+        <source>Second(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="130"/>
+        <source>Hot key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="141"/>
+        <source>F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="146"/>
+        <source>F2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="151"/>
+        <source>F3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="156"/>
+        <source>F4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="161"/>
+        <source>F5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="166"/>
+        <source>F6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="171"/>
+        <source>F7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="176"/>
+        <source>F8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="181"/>
+        <source>F9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="186"/>
+        <source>F10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="191"/>
+        <source>F11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mouseclickdialog.ui" line="196"/>
+        <source>F12</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
