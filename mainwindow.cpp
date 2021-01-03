acfe30eb689582d5b38f8003473efc9115e11381
tootal <tootal@yeah.net>
2021-01-03 13:28:42 +0800

    try load global hook

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 
 #include "mouseclickdialog.h"
+#include "GlobalHook/globalhook.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -26,8 +27,8 @@ void MainWindow::on_action_Mouse_Click_triggered()
 
 void MainWindow::on_action_About_triggered()
 {
-    auto dllname = "Test Lib";
-    auto v = 0x060201;
+    auto dllname = GlobalHook::name();
+    auto v = GlobalHook::version();
     auto dllverstr = QString("%3.%2.%1")
             .arg((v >> 0x0) & 0xFF)
             .arg((v >> 0x8) & 0xFF)
